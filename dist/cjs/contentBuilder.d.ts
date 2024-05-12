a492c68fc151457e93a81b5bc1d3c2e0252ffe45
123 <123@DESKTOP-2UK3QN5>
2024-05-12 11:57:20 +0200

    +dist

---
+++ dist/cjs/contentBuilder.d.ts
@@ -0,0 +1,4 @@
+import { ReactElement } from "react";
+import { ParamBuildContent } from "./menu.type";
+declare function buildContent(param: ParamBuildContent): ReactElement | undefined;
+export default buildContent;
